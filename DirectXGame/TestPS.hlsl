5e9d311d9039748fc64809c0d65046ece2910f7c
Deraserunaniko <k024g0076@g.neec.ac.jp>
2026-06-10 11:22:01 +0900

    ok

---
+++ DirectXGame/TestPS.hlsl
@@ -0,0 +1,10 @@
+struct PixeShaderOutput
+{
+    float4 color : SV_TARGET;
+};
+PixeShaderOutput main()
+{
+    PixeShaderOutput output;
+    output.color = float(1.0f, 1.0f, 1.0f, 1.0f);
+    return output;
+}
